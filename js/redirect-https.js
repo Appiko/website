d4324b11a8b041ffa2397f62ca1e19096ae2c20a
prithvirajnarendra <p@earthlord.io>
2018-07-24 16:02:56 +0530

    Remove the files from the previous template

--- js/redirect-https.js
+++
@@ -1,4 +0,0 @@
-var host = "appiko.org";
-if ((host == window.location.host) && (window.location.protocol != 'https:')) {
-	window.location = window.location.toString().replace(/^http:/, "https:");
-}
